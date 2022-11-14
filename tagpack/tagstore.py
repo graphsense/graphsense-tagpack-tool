d1be88ceb679becc064566d46e322a14820b4aa5
Gibran Gomez <gibran.gomez@imdea.org>
2022-11-14 18:36:57 +0100

    Changes to tagpack/tagstore.py suggested by flake8

--- tagpack/tagstore.py
+++ tagpack/tagstore.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 from datetime import datetime
 
 import numpy as np
@@ -58,18 +57,31 @@ class TagStore(object):
     def create_id(self, prefix, rel_path):
         return ":".join([prefix, rel_path]) if prefix else rel_path
 
-    def insert_tagpack(self, tagpack, is_public, force_insert, prefix, rel_path, batch=1000):
+    def insert_tagpack(self, tagpack, is_public, force_insert, prefix,
+                       rel_path, batch=1000):
+
         tagpack_id = self.create_id(prefix, rel_path)
         h = _get_header(tagpack, tagpack_id)
 
         if force_insert:
             print(f"evicting and re-inserting tagpack {tagpack_id}")
-            self.cursor.execute("DELETE FROM tagpack WHERE id = (%s)", (tagpack_id,))
-        self.cursor.execute("INSERT INTO tagpack (id, title, description, creator, uri, is_public) VALUES (%s,%s,%s,%s,%s,%s)", (h.get('id'), h.get('title'), h.get('description'), h.get('creator'), tagpack.uri, is_public))
+            q = "DELETE FROM tagpack WHERE id = (%s)"
+            self.cursor.execute(q, (tagpack_id,))
+
+        q = "INSERT INTO tagpack \
+            (id, title, description, creator, uri, is_public) \
+            VALUES (%s,%s,%s,%s,%s,%s)"
+        v = (h.get('id'), h.get('title'), h.get('description'),
+             h.get('creator'), tagpack.uri, is_public)
+        self.cursor.execute(q, v)
         self.conn.commit()
 
-        addr_sql = "INSERT INTO address (currency, address) VALUES (%s, %s) ON CONFLICT DO NOTHING"
-        tag_sql = "INSERT INTO tag (label, source, category, abuse, address, currency, is_cluster_definer, confidence, lastmod, context, tagpack ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        addr_sql = "INSERT INTO address (currency, address) VALUES (%s, %s) \
+            ON CONFLICT DO NOTHING"
+        tag_sql = "INSERT INTO tag (label, source, category, abuse, address, \
+            currency, is_cluster_definer, confidence, lastmod, \
+            context, tagpack ) VALUES \
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
         tag_data = []
         address_data = []
@@ -130,17 +142,23 @@ class TagStore(object):
         if update_existing:
             self.cursor.execute("SELECT address, currency FROM address")
         else:
-            self.cursor.execute("SELECT address, currency FROM address WHERE NOT is_mapped")
+            q = "SELECT address, currency FROM address WHERE NOT is_mapped"
+            self.cursor.execute(q)
         for record in self.cursor:
             yield record
 
     def insert_cluster_mappings(self, clusters):
         if not clusters.empty:
-            q = "INSERT INTO address_cluster_mapping (address, currency, gs_cluster_id , gs_cluster_def_addr , gs_cluster_no_addr )" \
-                "VALUES (%s, %s, %s, %s, %s) ON CONFLICT (currency, address) DO UPDATE SET " \
-                "gs_cluster_id = EXCLUDED.gs_cluster_id , gs_cluster_def_addr = EXCLUDED.gs_cluster_def_addr , gs_cluster_no_addr = EXCLUDED.gs_cluster_no_addr  "
+            q = "INSERT INTO address_cluster_mapping (address, currency, \
+                gs_cluster_id , gs_cluster_def_addr , gs_cluster_no_addr) \
+                VALUES (%s, %s, %s, %s, %s) ON CONFLICT (currency, address) \
+                DO UPDATE SET gs_cluster_id = EXCLUDED.gs_cluster_id, \
+                gs_cluster_def_addr = EXCLUDED.gs_cluster_def_addr, \
+                gs_cluster_no_addr = EXCLUDED.gs_cluster_no_addr"
 
-            data = clusters[['address', 'currency', 'cluster_id', 'cluster_defining_address', 'no_addresses']].to_records(index=False)
+            cols = ['address', 'currency', 'cluster_id',
+                    'cluster_defining_address', 'no_addresses']
+            data = clusters[cols].to_records(index=False)
 
             execute_batch(self.cursor, q, data)
             self.conn.commit()
@@ -149,7 +167,9 @@ class TagStore(object):
         return tag.all_fields.get('currency') in self.supported_currencies
 
     def finish_mappings_update(self, keys):
-        self.cursor.execute('UPDATE address SET is_mapped=true WHERE NOT is_mapped AND currency IN %s', (tuple(keys),))
+        q = 'UPDATE address SET is_mapped=true WHERE NOT is_mapped \
+                AND currency IN %s'
+        self.cursor.execute(q, (tuple(keys),))
         self.conn.commit()
 
     def get_ingested_tagpacks(self) -> list:
@@ -175,8 +195,10 @@ class TagStore(object):
             self.cursor.execute(query)
 
         keys = ['count', 'avg', 'stddev']
-        return {keys[i]:v for row in self.cursor.fetchall() \
-                    for i,v in enumerate(row)}
+        return {
+            keys[i]: v for row in self.cursor.fetchall()
+            for i, v in enumerate(row)
+        }
 
     def calculate_quality_measures(self) -> float:
         self.cursor.execute("CALL calculate_quality()")
@@ -191,9 +213,12 @@ def _get_tag(tag, tagpack_id):
 
     _, address = _get_currency_and_address(tag)
 
-    return (label, tag.all_fields.get('source'), tag.all_fields.get('category', None),
-            tag.all_fields.get('abuse', None), address, tag.all_fields.get('currency'),
-            tag.all_fields.get('is_cluster_definer'), tag.all_fields.get('confidence'),
+    return (label, tag.all_fields.get('source'),
+            tag.all_fields.get('category', None),
+            tag.all_fields.get('abuse', None), address,
+            tag.all_fields.get('currency'),
+            tag.all_fields.get('is_cluster_definer'),
+            tag.all_fields.get('confidence'),
             lastmod, tag.all_fields.get('context'), tagpack_id)
 
 
@@ -209,9 +234,6 @@ def _get_header(tagpack, tid):
     return {
         'id': tid,
         'title': tc['title'],
-#        'source': tc.get('source', os.path.split(tagpack.tags[0].all_fields.get('source'))[0]),
         'creator': tc['creator'],
         'description': tc.get('description', 'not provided'),
-#        'owner': tc.get('owner', 'unknown')
         }
-
